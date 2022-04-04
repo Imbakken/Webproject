d29bab1be1eae05e7a3f1efc21eb01d6bcc3b65e
rlangtinn95 <richalan@stud.ntnu.no>
2022-04-04 11:15:38 +0200

    endringer

---
+++ school_management/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const app = express();
+dotenv.config({path: 'config.env'});
+
+const PORT = process.env.PORT || 8080
+
+//logging requests
+
+app.use(morgan('tiny'));
+app.get('/', (req,res)=>{
+      res.send("app does work");
+})
+
+app.listen(3000, ()=> {(console.log(`Server is running on http://localhost:${3000}`))});
