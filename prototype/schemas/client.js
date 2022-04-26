1fd5d463637655d34e5eb904eaad36c27a4e4eef
rlangtinn95 <richalan@stud.ntnu.no>
2022-04-26 21:00:02 +0200

    changes

---
+++ prototype/schemas/client.js
@@ -0,0 +1,38 @@
+async function updateUser(firstName, middleName, surName, emailAdress, fieldOfEducation, degree, school, password){
+      var data = JSON.stringify({   "filter": {"firstname": firstName},
+                                               "middlename": middleName,
+                                               "surname": surName,
+                                               "email address": emailAdress,
+                                               "field of education": fieldOfEducation,
+                                               "degree": degree,
+                                               "school": school,
+                                               "password": password
+                                          });
+
+      let dataRecieved=""
+      await fetch(APIurl, {
+            method: "PUT", 
+            headers: {"Content-Type": "application/json"},
+            body: data
+      })
+      .then(resp => {
+            if (resp.status === 200){
+                  return resp.json();      
+            } else{
+                  console.log("Status" + resp.status);
+                  return Promise.reject("server");
+            }
+      })
+      .then(dataJson => {
+            dataRecieved = dataJson;
+      })
+      .catch(err => {
+            if (err === "server") return
+            console.log(err);
+      });
+
+      return dataRecieved;
+
+}      
+
+async function deleteUser(firstName)
