caf8f312db1561ea4dfa9c5da8dba7f71c773771
Imbakken <imbakken@stud.ntnu.no>
2022-04-26 21:07:19 +0200

    .

--- prototype/server/server.js
+++ prototype/server/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 //app.use('/auth', auth);
 
-app.use("/users", require("../routes/userRoutes"));
+app.use("/users", require("./routes/userRoutes"));
 
 app.listen(3000, () => {
   console.log(`server working and listening at ${PORT}`);
