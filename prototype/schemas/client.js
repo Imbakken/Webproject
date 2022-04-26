4931c6a7e7ec2210cdee2c3c9acca7ca90cc90f9
rlangtinn95 <richalan@stud.ntnu.no>
2022-04-26 20:32:13 +0200

    client js updated

--- prototype/schemas/client.js
+++ prototype/schemas/client.js
@@ -9,6 +9,28 @@ async function updateUser(firstName, middleName, surName, emailAdress, fieldOfEd
                                                "password": password
                                           });
 
-      let dataRecieved
+      let dataRecieved=""
+      await fetch(APIurl, {
+            method: "PUT", 
+            headers: {"Content-Type": "application/json"},
+            body: data
+      })
+      .then(resp => {
+            if (resp.status === 200){
+                  return resp.json();      
+            } else{
+                  console.log("Status" + resp.status);
+                  return Promise.reject("server");
+            }
+      })
+      .then(dataJson => {
+            dataRecieved = dataJson;
+      })
+      .catch(err => {
+            if (err === "server") return
+            console.log(err);
+      });
+
+      return dataRecieved;
 
 }      
