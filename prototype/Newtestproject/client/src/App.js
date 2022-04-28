4cccae843b157f1d44aafa9eda8fb8f2e19d30c2
Imbakken <imbakken@stud.ntnu.no>
2022-04-28 21:33:37 +0200

    .

--- prototype/Newtestproject/client/src/App.js
+++ prototype/Newtestproject/client/src/App.js
@@ -1,47 +1,52 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './setAuthToken';
-import { setCurrentUser, logoutUser } from './actions/authentication';
-
-import Navbar from './components/Navbar';
-import Register from './components/Register';
-import Login from './components/Login';
+import * as React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import { AuthConsumer } from './utils/AuthContextProvider';
+import ProtectedRoute from './components/routes/ProtectedRoute';
 import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import Admin from './components/Admin';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
+import Logout from './components/Logout';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-if(localStorage.jwtToken) {
-  setAuthToken(localStorage.jwtToken);
-  const decoded = jwt_decode(localStorage.jwtToken);
-  store.dispatch(setCurrentUser(decoded));
+function App() {
+  return (
+    <div className="App">
+      <AuthConsumer>
+        {({ isAuth }) => (
+          <>
+            {/* TIP: replaced by specific component for navigation */}
+            <header className="App-header">
+              {/* TIP: this is not good in terms of accessibility. We should use nav + ul>li  */}
+              <Link to="/">Public</Link>
 
-  const currentTime = Date.now() / 1000;
-  if(decoded.exp < currentTime) {
-    store.dispatch(logoutUser());
-    window.location.href = '/login'
-  }
-}
+              {/* Now we can use the AuthConsumer to know if the user is authenticated and show specific elements in the menu */}
+              {isAuth && <Link to="/dashboard">Dashboard</Link>}
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store = { store }>
-        <Router>
-            <div>
-              <Navbar />
-                <Route exact path="/" component={ Home } />
-                <div className="container">
-                  <Route exact path="/register" component={ Register } />
-                  <Route exact path="/login" component={ Login } />
-                </div>
-            </div>
-          </Router>
-        </Provider>
-    );
-  }
+              {!isAuth && <Link to="/login">Log in</Link>}
+
+              {/* Only show link if authenticated user */}
+              {isAuth && <Link to="/logout">Log out</Link>}
+            </header>
+            <main>
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="logout" element={<Logout />} />
+                <Route element={<ProtectedRoute />}>
+                  <Route path="dashboard" element={<Dashboard />} />
+                  <Route path="admin" element={<Admin />} />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+          </>
+        )}
+      </AuthConsumer>
+    </div>
+  );
 }
 
 export default App;
--- prototype/Newtestproject/client/src/index.js
+++ prototype/Newtestproject/client/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client"; //since react 18
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './utils/AuthContextProvider';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//since react 18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// 1) install router and wrap app with BrowserRouter
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
 
