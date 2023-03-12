c7db9a73c83afe24419b454450d5450b959fe3fd
Imbakken <imbakken@stud.ntnu.no>
2023-03-12 19:29:18 +0100

    .

--- server.js
+++ server.js
@@ -53,12 +53,12 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 }); */
 
-
-app.listen(port, () => console.log(`Server is up and running on port ${port}`));
-
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
     app.use(express.static('client/build'));
 
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
   }
+
+app.listen(port, () => console.log(`Server is up and running on port ${port}`));
+
